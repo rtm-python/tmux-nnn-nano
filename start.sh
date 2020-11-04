1f88a7aaff62fd93ff062b2e401d3276a95ebe68
Rollan TAIGULOV <rt@localhost.localdomain>
2020-11-05 03:45:44 +0600

    Update start script.

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 tmux new-session \; \
-	send-keys 'nnn ~' C-m \; \
-	split-window -v -p 30  \; \
+	send-keys 'nnn ~ -f -d -o -e' C-m \; \
+	split-window -v -p 25  \; \
 	send-keys 'cd ~' C-m \; \
 	send-keys 'clear' C-m \; \
 	select-pane -t 0 \; \
-	split-window -h -p 70 \; \
+	split-window -h -p 75 \; \
+	send-keys 'cd ~' C-m \; \
+	send-keys 'clear' C-m \; \
+	split-window -h -p 50 \; \
 	send-keys 'cd ~' C-m \; \
 	send-keys 'clear' C-m \;
+
