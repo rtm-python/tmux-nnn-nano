6aff397983477aee882780fa93388007e381a510
Rollan TAIGULOV <rt@localhost.localdomain>
2021-01-18 22:03:26 +0600

    Rearrange window spaces

--- start.sh
+++ start.sh
@@ -13,11 +13,11 @@ export VISUAL=$(pwd)/nnn-edit
 
 tmux \
 	new-session -s "$session" \; \
-	send-keys 'nnn '"$initdir"' -f -d -o -e' Enter \; \
-	split-window -c "$initdir" -v -p 25  \; \
+	send-keys 'nnn '"$initdir"' -f -o -e' Enter \; \
+	split-window -c "$initdir" -v -p 20  \; \
 	select-pane -t 0 \; \
-	split-window -c "$initdir" -h -p 75 \; \
-	split-window -c "$initdir" -h -p 50 \; \
+	split-window -c "$initdir" -h -p 80 \; \
+	split-window -c "$initdir" -h -p 40 \; \
 	set-option -g mouse on \; \
 	set-option -g prefix C-a \; unbind-key C-b \; \
 	bind Escape copy-mode \; unbind [ \; \
